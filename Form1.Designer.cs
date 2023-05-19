d72c39facb87d40a331935e92a249daa5409a19a
eda oncel <edaoncel@email.com>
2023-05-19 14:38:36 +0300

    Program Yükleniyor Projesi

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace program_yukleniyor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pcbresim = new System.Windows.Forms.PictureBox();
+            this.pbdurum = new System.Windows.Forms.ProgressBar();
+            this.lbldurum = new System.Windows.Forms.Label();
+            this.btndevam = new System.Windows.Forms.Button();
+            this.tmryukleyici = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbresim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbresim
+            // 
+            this.pcbresim.Location = new System.Drawing.Point(172, 47);
+            this.pcbresim.Name = "pcbresim";
+            this.pcbresim.Size = new System.Drawing.Size(493, 90);
+            this.pcbresim.TabIndex = 0;
+            this.pcbresim.TabStop = false;
+            // 
+            // pbdurum
+            // 
+            this.pbdurum.Location = new System.Drawing.Point(172, 155);
+            this.pbdurum.Name = "pbdurum";
+            this.pbdurum.Size = new System.Drawing.Size(493, 33);
+            this.pbdurum.TabIndex = 1;
+            // 
+            // lbldurum
+            // 
+            this.lbldurum.AutoSize = true;
+            this.lbldurum.Location = new System.Drawing.Point(168, 212);
+            this.lbldurum.Name = "lbldurum";
+            this.lbldurum.Size = new System.Drawing.Size(153, 20);
+            this.lbldurum.TabIndex = 2;
+            this.lbldurum.Text = "birazdan baslayacak";
+            // 
+            // btndevam
+            // 
+            this.btndevam.Location = new System.Drawing.Point(559, 298);
+            this.btndevam.Name = "btndevam";
+            this.btndevam.Size = new System.Drawing.Size(106, 52);
+            this.btndevam.TabIndex = 3;
+            this.btndevam.Text = "devam-->";
+            this.btndevam.UseVisualStyleBackColor = true;
+            this.btndevam.Click += new System.EventHandler(this.btndevam_Click);
+            // 
+            // tmryukleyici
+            // 
+            this.tmryukleyici.Interval = 1000;
+            this.tmryukleyici.Tick += new System.EventHandler(this.tmryukleyici_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btndevam);
+            this.Controls.Add(this.lbldurum);
+            this.Controls.Add(this.pbdurum);
+            this.Controls.Add(this.pcbresim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbresim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbresim;
+        private System.Windows.Forms.ProgressBar pbdurum;
+        private System.Windows.Forms.Label lbldurum;
+        private System.Windows.Forms.Button btndevam;
+        private System.Windows.Forms.Timer tmryukleyici;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program_yukleniyor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmryukleyici.Start();
+        }
+
+        private void tmryukleyici_Tick(object sender, EventArgs e)
+        {
+            Random rastgeleSayiOlusturucu = new Random();
+            int artisOranı = rastgeleSayiOlusturucu.Next(1, 20);
+            pbdurum.Step = artisOranı;
+            int suAnkiDeger = pbdurum.Value;
+            if(suAnkiDeger<=20)
+            {
+                lbldurum.Text = "Program yüklemeye hazırlanıyor.";
+            }
+            else if (suAnkiDeger <= 20 && suAnkiDeger <= 45)
+            {
+                lbldurum.Text = "güvenlik modülü yükleniyor.";
+            }
+            else if (suAnkiDeger <= 45 && suAnkiDeger <= 70 )
+            {
+                lbldurum.Text = "veritabanı modülü yükleniyor.";
+            }
+            else if (suAnkiDeger <= 70 && suAnkiDeger <= 95)
+            {
+                lbldurum.Text = "bellek optimizasyonu yapılıyor.";
+            }
+            else if (suAnkiDeger <= 95 && suAnkiDeger <= 100)
+            {
+                lbldurum.Text = "lazer klkanları devre dışı bırakılıyor.";
+            }
+            else 
+            {
+                tmryukleyici.Stop();
+                btndevam.Enabled = true;
+            }
+        }
+
+        private void btndevam_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
